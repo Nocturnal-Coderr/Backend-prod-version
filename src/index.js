5944bffc1adeca6c2eab80614deb02f2f97c66c5
Ashutosh Srivastava <ashutoshsrivastava9795@gmail.com>
2024-11-03 18:09:16 +0530

    util and middleware setup with error handling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,24 @@ dotenv.config({
     path: './.env'
 })
 
+
+
 connectDB()
+.then(() => {
+
+    app.on("error", (error) => {
+        console.log("ERR: ", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is unning at PORT : ${precess.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed : Connection error !!!", err);
+    
+})
 
 
 
