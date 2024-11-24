cdc58230467276b212db7011fd0f95a5d0a023c5
Ashutosh Srivastava <ashutoshsrivastava9795@gmail.com>
2024-11-24 21:12:59 +0530

    added authentication and tokens for access.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
@@ -16,7 +17,7 @@ connectDB()
     })
 
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`server is unning at PORT : ${precess.env.PORT}`);
+        console.log(`server is running at PORT : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
